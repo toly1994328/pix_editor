eb6008eb783b58831fb48e6f401a96ed59c32e9f
toly <1981462002@qq.com>
2024-04-11 09:44:50 +0800

    v1.2.0

---
+++ lib/app/res/toly_icon.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/widgets.dart';
+// Power By 张风捷特烈--- Generated file. Do not edit.
+// 欢迎支持： https://github.com/toly1994328/FlutterUnit
+class TolyIcon {
+    TolyIcon._();
+    static const IconData iconSelector = IconData(0xe864, fontFamily: "TolyIcon");
+static const IconData iconPainter = IconData(0xe665, fontFamily: "TolyIcon");
+static const IconData iconInfo = IconData(0xe636, fontFamily: "TolyIcon");
+static const IconData iconSave = IconData(0xe605, fontFamily: "TolyIcon");
+static const IconData iconClear = IconData(0xe629, fontFamily: "TolyIcon");
+
+}  
